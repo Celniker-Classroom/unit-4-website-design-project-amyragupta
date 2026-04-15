ead5edae88dd17c9d8945d95801c04182698c070
amyragupta <guptaa4666@my.sduhsd.net>
2026-04-15 18:04:41 +0000

    workday8

--- interaction.js
+++ interaction.js
@@ -1,3 +1,4 @@
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,4 +26,24 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
